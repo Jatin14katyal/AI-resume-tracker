a6f21fc2e3a7b46e1c9b13ef8ead18fbd8502a30
Jatin14katyal <katyalsaab496@gmail.com>
2026-03-12 23:38:21 +0530

    Navbar update

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -3,7 +3,7 @@
   return (
    <nav className="navbar">
     <Link to="/">
-    <p className="text-2xl font-bold text-gradient">RESUMIND do</p></Link>
+    <p className="text-2xl font-bold text-gradient">RESUMIND</p></Link>
 
    </nav>
   )
